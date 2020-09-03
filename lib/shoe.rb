1dc6327c73cb3a2eb1985f379a60ad81df157599
Jeremy Wood <js.wood206@gmail.com>
2020-09-03 04:32:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class shoe
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "The shoe has been repaired."
+    @condition = "new"
+
+
+end
