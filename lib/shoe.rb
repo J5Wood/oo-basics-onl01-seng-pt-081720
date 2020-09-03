3a926ae23aedcdacfd285646dac77180cc0e629d
Jeremy Wood <js.wood206@gmail.com>
2020-09-03 04:35:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,10 @@
 class Shoe
 
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
-
+  initialize(brand)
+    @brand =  brand
+  end
 
 end
