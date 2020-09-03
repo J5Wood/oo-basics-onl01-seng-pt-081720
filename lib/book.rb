fba34a8767e28315b598c50abfaeb4a23476bb52
Jeremy Wood <js.wood206@gmail.com>
2020-09-03 04:09:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,7 @@
 class Book
 
-  def initialize(title, author)
+  def initialize(title)
     @title = title
-    @author = author
   end
 
   def title
@@ -10,7 +9,6 @@ class Book
   end
 
   def author
-    @author
   end
 
 end
